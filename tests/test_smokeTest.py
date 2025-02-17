41d388ffd50bb82e8078547548f26ee192dd412a
Ethem-deli <del22021@byui.edu>
2025-02-17 19:08:09 +0300

    Commint Smoke Tests (Host adjusted)

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -22,7 +22,7 @@ class TestSmokeTest():
     self.driver.quit()
   
   def test_adminPage(self):
-    self.driver.get("http://192.168.18.11:5500/teton/1.6/admin.html")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1221, 1032)
     self.driver.find_element(By.LINK_TEXT, "Admin").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(2)").text == "Username:"
@@ -36,7 +36,7 @@ class TestSmokeTest():
     WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
   def test_directoryPage(self):
-    self.driver.get("http://192.168.18.11:5500/teton/1.6/index.html")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1190, 1032)
     self.driver.find_element(By.LINK_TEXT, "Directory").click()
     self.driver.find_element(By.ID, "directory-grid").click()
@@ -49,7 +49,7 @@ class TestSmokeTest():
     assert len(elements) > 0
   
   def test_homePage(self):
-    self.driver.get("http://192.168.18.11:5500/teton/1.6/index.html")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
     assert len(elements) > 0
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
@@ -65,7 +65,7 @@ class TestSmokeTest():
     self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
   
   def test_joinPage(self):
-    self.driver.get("http://192.168.18.11:5500/teton/1.6/index.html")
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1190, 1032)
     self.driver.find_element(By.LINK_TEXT, "Join").click()
     assert self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(2)").text == "First Name"
